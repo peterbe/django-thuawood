885323cb27dbefbcc7c09fea80d75c3df9a53be1
Peter Bengtsson <mail@peterbe.com>
2011-12-04 19:59:09 -0800

    babys step start

---
+++ README.md
@@ -0,0 +1,5 @@
+django-thuawood
+===============
+
+A website for my mom. www.thuawood.com
+
---
+++ thuawood/settings/__init__.py
@@ -0,0 +1,4 @@
+from .base import *
+from .local import *
+
+assert SECRET_KEY, "must set your own SECRET_KEY in settings/local.py"
