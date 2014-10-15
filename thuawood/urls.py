cbe0e9e1cc11a745200e0103b6e6a425b76edb16
Peter Bengtsson <mail@peterbe.com>
2014-10-14 22:18:40 -0700

    some necessary fixes

--- thuawood/urls.py
+++ thuawood/urls.py
@@ -1,5 +1,6 @@
-from django.conf.urls.defaults import patterns, include, url
+from django.conf.urls import patterns, include, url
 from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -14,3 +15,6 @@ urlpatterns = patterns('',
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
+
+if settings.DEBUG:
+    urlpatterns += staticfiles_urlpatterns()
